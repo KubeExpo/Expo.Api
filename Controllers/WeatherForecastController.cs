402df261c16de442a5827107972e3cb726d55a3d
Joydeep <jrc.dxc@gmail.com>
2019-12-16 23:13:05 +0530

    Added today action to forecast controller to test ingress

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -46,19 +46,14 @@ namespace Expo.Api.Controllers
         public IActionResult Today()
         {
             var rng = new Random();
-            WeatherForecastV2 weatherForecast = new WeatherForecastV2
+            List<WeatherForecast> WeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Version = "v2",
-                WeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Version = "v2",
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                }).ToList()
-
-            };
-            return Ok(weatherForecast);
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToList();
+            return Ok(WeatherForecasts);
         }
     }
 }
