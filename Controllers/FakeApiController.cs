6bd305ad54c8ba237e773503b111fc7e564552a2
joydeep <jrc.dxc@gmail.com>
2020-01-21 15:06:20 +0530

    Added a new api /company/employee

--- Controllers/FakeApiController.cs
+++ Controllers/FakeApiController.cs
@@ -7,9 +7,9 @@ namespace Expo.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class FakeApiController: ControllerBase
+    public class FakeApiController : ControllerBase
     {
-        HttpClient client ;
+        HttpClient client;
         public FakeApiController()
         {
             client = new HttpClient();
@@ -18,11 +18,19 @@ namespace Expo.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            
             client.BaseAddress = new Uri("https://randomuser.me");
             HttpResponseMessage responseMessage = await client.GetAsync("/api/?results=1");
             string responseString = await responseMessage.Content.ReadAsStringAsync();
             return Ok(responseString);
-        }   
+        }
+
+        [HttpGet("company/employee")]
+        public async Task<IActionResult> GetEmployee()
+        {
+            client.BaseAddress = new Uri("http://dummy.restapiexample.com");
+            HttpResponseMessage responseMessage = await client.GetAsync("/api/v1/employees");
+            string responseString = await responseMessage.Content.ReadAsStringAsync();
+            return Ok(responseString);
+        }
     }
 }
